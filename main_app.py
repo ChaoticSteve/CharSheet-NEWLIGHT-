c9d485e6b2ca1af0c81d94b5408134a7ff1c68fd
Tyunin Pavel <tuninpavel@gmail.com>
2022-10-12 12:59:23 +0600

    optimise

--- main_app.py
+++ main_app.py
@@ -1,64 +1,44 @@
-from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import (QApplication, QWidget, QHBoxLayout, QVBoxLayout, QGroupBox, QRadioButton,
-                             QPushButton, QLabel, QListWidget, QLineEdit)
+from PyQt6.QtCore import Qt, QEvent
+from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout, QHBoxLayout, QRadioButton, \
+    QMessageBox
+from PyQt6 import uic
+from time import sleep
+from random import randint
 
-from instr import *
+Form, Window = uic.loadUiType("gui.ui")
 
 
-class MainWin(QWidget):
+class Logic(Form):
     def __init__(self):
         super().__init__()
-        self.set_apper()
-        self.initUI()
+
+        self.window = Window()
+        self.setupUi(self.window)
         self.connects()
-        self.show()
-
-    def set_apper(self):
-        self.setWindowTitle(win_title)
-        self.resize(win_w, win_h)
-
-    def initUI(self):
-        self.h_line = QHBoxLayout()
-        self.v_line1 = QVBoxLayout()
-        self.v_line1.addWidget(QLabel('Навыки'), alignment=Qt.AlignCenter)
-        self.v_line1.addWidget(QLabel('Имя'))
-        self.v_line1.addWidget(QLabel('Раса'))
-        self.v_line1.addWidget(QLabel('Очки Развития'))
-        self.v_line1.addWidget(QLabel('Репутация'))
-        self.v_line1.addWidget(QLabel('Деньги'))
-        self.v_line1.addWidget(QLabel('Сила'))
-        self.v_line1.addWidget(QLabel('Телосложение'))
-        self.v_line1.addWidget(QLabel('Ловкость'))
-        self.v_line1.addWidget(QLabel('Харизма'))
-        self.v_line1.addWidget(QLabel('Интеллект'))
-        self.v_line1.addWidget(QLabel('Воля'))
-        self.v_line1.addWidget(QLabel('Очки характиристик'))
-        self.v_line1.addWidget(QLabel('Очки навыков'))
-        self.v_line1.addWidget(QLabel('Здоровье'))
-        self.v_line1.addWidget(QLabel('Бодрость'))
-        self.v_line1.addWidget(QLabel('Инициатива'))
-        self.v_line1.addWidget(QLabel('Скорость'))
-        self.v_line1.addWidget(QLabel('Край жизни'))
-        self.h_line.addLayout(self.v_line1)
-        self.v_line2 = QVBoxLayout()
-        self.name = QLineEdit('Имя персонажа')
-        self.race = QLineEdit('Раса')
-        self.ev_point = QLineEdit('Очки развития')
-        self.reputation = QLineEdit('Репутация')
-        self.money = QLineEdit('Деньги')
-        self.v_line2.addWidget(self.name)
-        self.v_line2.addWidget(self.race)
-        self.v_line2.addWidget(self.ev_point)
-        self.v_line2.addWidget(self.reputation)
-        self.v_line2.addWidget(self.money)
-        self.h_line.addLayout(self.v_line2)
-        self.setLayout(self.h_line)
-
+        self.events()
+        self.window.show()
 
     def connects(self):
         pass
 
+    def events(self):
+        self.dogfight_btn.mousePressEvent = self.check
+        self.dogfight_btn.mouseReleaseEvent = self.draw
+
+    def check(self, *args):
+        self.result.setText('Рукопашный бой')
+        self.dogfight_btn.setStyleSheet("color : white;"
+                                        "background-color : blue;")
+
+
+    def draw(self, *args):
+        self.dogfight_btn.setStyleSheet("background-color : none")
+
 
 app = QApplication([])
-mw = MainWin()
-app.exec_()
+#window = Window()
+form = Logic()
+# form.setupUi(window)
+
+#window.show()
+app.exec()
